125ae12cf080c9059ff514549fbd5b7029d9a548
tarunlalchandani <tarunlalchandani4236562@gmail.com>
2020-06-13 15:58:08 +0530

    Producthunt Completion

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -121,5 +121,8 @@ USE_TZ = True
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = "/static/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
